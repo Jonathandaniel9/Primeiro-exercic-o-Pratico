188793d0ef64b087d2c47bf092215cf5d8c6680e
Jonathan Daniel Da Silva <112485639+Jonathandaniel9@users.noreply.github.com>
2022-09-06 22:04:49 -0300

    Add files via upload

---
+++ indexcwi.js
@@ -0,0 +1,10 @@
+var nomeCompleto = 'Jonathan Daniel da Silva'
+var apelido = 'dioninha'
+var idade = '29'
+var dataNascimento = '09/12/1992'
+var localNascimento = 'Taquara-RS'
+var altura = '1.68'
+var trabalhando = true
+var apresentacao = "Meu nome é " + nomeCompleto + " (sou conhecido como " + apelido + ") e tenho " + idade + " anos. Nasci no dia " + dataNascimento + ", na cidade de " + localNascimento + ". Tenho " + altura + "m de altura e atualmente estou " + (trabalhando ? "empregado" : "desempregado") + "."
+console.log(apresentacao)
+
